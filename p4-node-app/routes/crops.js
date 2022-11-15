58d36591b1d71ffc412a17e3361ac0203b6203bb
jrosi <j.rosimo098@gmail.com>
2022-11-15 08:31:07 +0800

    Feat: added and tested post and delete of crops

--- p4-node-app/routes/crops.js
+++ p4-node-app/routes/crops.js
@@ -4,7 +4,7 @@ const router = express.Router();
 //Model
 const Crop = require("../models/Crop");
 
-//Create new crop
+//Create new crop from a user ------(TEST OK)--------
 router.post("/:id", (request, response) => {
   let newCrop = new Crop({
     ...request.body,
@@ -15,7 +15,7 @@ router.post("/:id", (request, response) => {
   });
 });
 
-//Delete a crop
+//Delete a crop ------(TEST OK)-------
 router.delete("/:id", (request, response) => {
   Crop.deleteOne({ _id: request.params.id }).then((result) => {
     if (result.deletedCount === 1) {
